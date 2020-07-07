181ca975c64bdc020ddf23baec46907aec7be0b8
Brian Hsu <brianhsu.hsu@gmail.com>
2020-07-07 09:50:49 +0800

    add: Add test scenario for edit project.

--- src/test/scala/moe/brianhsu/maidroidtask/usecase/project/EditProjectTest.scala
+++ src/test/scala/moe/brianhsu/maidroidtask/usecase/project/EditProjectTest.scala
@@ -14,5 +14,59 @@ class EditProjectTest extends BaseFixtureFeature[EditProjectFixture] {
       Then("it should NOT pass the validation and yield NotFound error")
       pending
     }
+
+    Scenario("Edit a project that belongs to other use") { fixture =>
+      Given("user request to edit a project that belongs to others")
+      When("run the use case")
+      Then("it should NOT pass the validation and yield AccessDenied error")
+      pending
+    }
+
+    Scenario("Edit a project that has duplicate name for same user") { fixture =>
+      Given("user request to edit a project and new name is duplicated with user's other project")
+      When("run the use case")
+      Then("it should NOT pass the validation and yield Duplicate error")
+      pending
+    }
+
+    Scenario("Edit a project that child-project does not exist") { fxiture =>
+      Given("user request to edit a project that new child project is not exist")
+      When("run the use case")
+      Then("it should NOT pass the validation and yield NotFound error")
+      pending
+    }
+
+    Scenario("Edit a project that child-project is belongs to other user") { fixture =>
+      Given("user request to edit a project that new child project is belongs to other user")
+      When("run the use case")
+      Then("it should NOT pass the validation and yield AccessDenied error")
+      pending
+    }
+
+    Scenario("Edit a project that name is duplicated with trashed project") { fixture =>
+      Given("user request to edit a project")
+      And("new project name is duplicated with trashed project")
+      When("run the use case")
+      Then("it should pass the validation")
+      pending
+    }
+
+    Scenario("Edit a project that name is duplicate with other user's project") { fixture =>
+      Given("user request to edit a project")
+      And("new project name is duplicate with other user's project")
+      When("it should pass the validation")
+      pending
+    }
+  }
+
+  Feature("Store edited project in storage") {
+    Scenario("Edit a project with validate request") { fixture =>
+      Given("a exist project in system")
+      And("user request to edit that project with new attributes")
+      When("run the request")
+      Then("it should return edited project")
+      And("store it in storage")
+      And("generate correct journal entry")
+    }
   }
 }
